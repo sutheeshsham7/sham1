5c4ee4d7ee391f3ac153d8b45989e5aa3131adb5
sutheeshsham7 <42170136+sutheeshsham7@users.noreply.github.com>
2018-08-28 13:46:28 +0530

    Create test4.java

---
+++ test4.java
@@ -0,0 +1,14 @@
+public class test4
+{
+public static void main (String args[])
+{
+char c='*';
+if((c>='a'&&c<='z')||(c>='A'&& c<='Z'))
+{
+System.out.println(c + "is an alphabet");
+}
+else
+{
+System.out.println(c +"is not an alphabet");
+}
+}}
