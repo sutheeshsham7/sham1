4f876b0a9b8ac470fec09de809b5b08ed0fe2a6a
sutheeshsham7 <42170136+sutheeshsham7@users.noreply.github.com>
2018-09-06 11:46:06 +0530

    Create Ideone.java

---
+++ Ideone.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for (int i = 0; i < 10; i++)
+    {
+			System.out.println("Hello World");
+		}
+	}
+}
